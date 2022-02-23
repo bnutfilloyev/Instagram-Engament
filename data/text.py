203f1c75bec02f0f31dfc6438cd3211f7d370d9d
yoshlikmedia <yoshlikmedia@gmail.com>
2022-02-24 00:22:30 +0500

    update texts

--- data/text.py
+++ data/text.py
@@ -1,37 +1,88 @@
 text = {
-    'start_text': 'Welcome to @zoe_engagementbot 🤘\n\n'
+    'start_text': 'Welcome to @code_testing_bot 🤘\n\n'
                   '🔥 Get more likes by joining and participating in All Seasons engagement groups👇\n\n'
                   '- LIKES\n'
-                  '🔸Dx30: @summerdx30\n'
-                  '🔸Dx25: @winterdx25\n'
-                  '🔸Dx20: @summerdx20\n'
-                  '🔸Dx15: @winterdx15\n\n'
+                  '🔸 Dx30: https://t.me/+eyWGA9NXDbgzZDUy\n'
                   '🍁 Contest alert 🍁\n'
-                  '👤Top 2 Users who engage the most will get 2 weeks free autodrop!\n'
+                  '👤 Top 2 Users who engage the most will get 2 weeks free autodrop!\n'
                   'Stay tuned for exciting news xX \n\n'
                   '💥 Tired of Engaging 💥\n'
                   'Join our premium services and post without engaging back❗️\n'
                   'Also get auto likes and comments every time you post from our bot. \n\n'
                   'Contact admin @yoshlik_media for prices.\n\n'
-                  'Regards xX\n'
-                  '📸  Bot created using:  https://EngageBot.PRO',
+                  'Regards DY\n'
+                  '📸 Bot created using:  @yoshlik_media\n',
 
     'help_text': '👋 Hello, and welcome to the  HELP SECTION  of this bot!\n\n\n'
-                 '❤️ Here you can receive more information about https://engagebot.pro, the  '
+                 '❤️ Here you can receive more information about WEBSITE URL, the  '
                  '<b>Advanced Engagement Group Management System ,</b> '
                  'developed by AlexDev and acquired by EngageBot S.r.l.\n\n'
                  '📋 <b> Main bot commands:</b> \n'
                  '- /help | Show this section\n'
                  '- /list | Show the complete list of posts to retrieve\n'
                  '- /rules | Show the rules of the group\n\n'
-                 '👮🏻‍♂ <b> Group admin commands: </b/>\n'
-                 '➡️ https://t.me/EngageBotPRO/102\n\n'
+                 '👮🏻‍♂ <b> Group admin commands: </b>\n'
+                 '➡️ URL \n\n'
                  '<b>❓  Do you like this bot, and would like to have one customized for your group?</b>\n'
                  '❕ Check out these links!\n\n'
                  '🤖  Information Bot: '
-                 '@EngageProBot\n\n'
+                 '@URL\n\n'
                  '📣  Channel Updates: \n'
-                 '@EngageBotPro\n\n'
+                 '@URL\n\n'
                  '🌐  Official Website: \n'
-                 'https://engagebot.pro\n\n'
+                 'WEBSITE URL\n\n',
+
+    'wait_text': 'Please wait...',
+
+    'warning_text': "⚠️ Warning {}, the correct format to use to send your photo is: <b>Dx30 PhotoLink</b>",
+
+    'instagram_text': "👤 {} ✅ {} ({}) \n"
+                      "📸 {}",
+
+    'instagram_button': '📸 Photo 📸',
+
+    'instagram_link_list': "📝 List 📝",
+
+    'checking_list': "🔎 {}, i'm checking if you correctly engaged",
+
+    'list_text': '❗️ Engage with the following photos, then go back and post your link!\n\n',
+
+    'get_views': '',
+    
+    'rules': "1️⃣ Start @REACHBOOST (Click on Send Message, Click Start) \n" 
+             "2️⃣ Go back to this group and type /list to get your links to catchup \n" 
+             "3️⃣ Engage with all the Instagram links the bot sent you \n" 
+             "4️⃣ When you are done, you can post your link in the following format: \n\n" 
+             "Dx @instagramusername\n" 
+             "https://www.instagram.com/p/xxxxxxxxxxxx/\n" 
+             "⚠️ ONLY users who engaged correctly will be added to the engagement list. " 
+             "If you don't engage correctly, our bot will repost your post but not add it " 
+             "to the engagement list. If you forgot to catch up on some posts, " 
+             "this chat will give you the list of missed posts! ⚠\n\n️" 
+             "👉 Dx means „Done x“\n" 
+             "👉 Follow all of these rules to prevent geting warned and banned\n" 
+             "👉 5 warns results in a ban\n" 
+             "👉 Make sure your account is public\n" 
+             "👉 Make sure your link and username are valid\n" 
+             "👉 Be kind to everyone",
+
 }
+
+views_button = [
+    {
+        "callback": "views:1",
+        "title": "1K",
+    },
+    {
+        "callback": "views:2",
+        "title": "2K",
+    },
+    {
+        "callback": "views:3",
+        "title": "3K",
+    },
+    {
+        "callback": "views:4",
+        "title": "4K",
+    }
+]
